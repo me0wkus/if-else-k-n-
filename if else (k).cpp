0e54bfc256552bbe5503350eabf15cd203cb5331
me0wkus <118027059+me0wkus@users.noreply.github.com>
2022-11-12 20:53:39 +0300

    Add files via upload

---
+++ if else (k).cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	setlocale(0, "");
+	double n;
+	int k;
+
+	cout << "Enter the operator to perform: ";
+	cin >> n;
+
+	if (n < 10)
+		cout << "This number is lower than 10" << endl;
+	k = 0;
+
+		else if (n == 10) {
+			cout << "This number is 10" << endl;
+			k = 10;
+		}
+		else {
+			cout << "This number is more than 10" << endl;
+			k = 1;
+		}
+	}
+	cout << "k = " << endl;
+	return 0;
+}
